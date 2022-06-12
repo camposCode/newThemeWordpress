062294e7513eca5f6632e82aea5ef3263a10023e
camposCode <info@josecampos.io>
2022-06-12 15:46:32 +0200

    individual posts created

---
+++ app/public/wp-content/themes/simple/footer.php
@@ -0,0 +1,11 @@
+<footer>
+        <div class="container">
+            <p>&copy; <?php bloginfo('name'); ?> - <?php the_time('Y'); ?></p>
+        </div>
+
+    </footer>
+
+    <?php wp_footer(); ?>
+</body>
+
+</html>
--- app/public/wp-content/themes/simple/index.php
+++ app/public/wp-content/themes/simple/index.php
@@ -1,31 +1,25 @@
 <?php get_header(); ?>
 
-    <div class="main">
-        <div class="container">
-            <?php if (have_posts()) : ?>
-            <?php while(have_posts()): the_post();?>
-            <h3><?php the_title(); ?></h3>
+<div class="main">
+    <div class="container">
+        <?php if (have_posts()) : ?>
+        <?php while(have_posts()): the_post();?>
+
+        <article class="post">
+            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> </h3>
             <?php the_content(); ?>
+        </article>
 
-            <div class="meta">
-                Created by <?php the_author(); ?> on <?php the_date(); ?>
-            </div>
-            <?php endwhile; ?>
-            <?php else: ?>
-            <?php echo wpautop('Sorry, not posts were found'); ?>
-            <?php endif;  ?>
+
+        <div class="meta">
+            Created by <?php the_author(); ?> on <?php the_date(); ?>
         </div>
-
+        <?php endwhile; ?>
+        <?php else: ?>
+        <?php echo wpautop('Sorry, not posts were found'); ?>
+        <?php endif;  ?>
     </div>
 
-    <footer>
-        <div class="container">
-            <p>&copy; <?php bloginfo('name'); ?> - <?php the_time('Y'); ?></p>
-        </div>
+</div>
 
-    </footer>
-
-    <?php wp_footer(); ?>
-</body>
-
-</html>
+<?php get_footer(); ?>
